20cffc655798a8c40284b8cd4eca6fdc7f287f03
macvegas <macvegas7@gmail.com>
2017-08-02 14:24:37 +0200

    first commit

---
+++ app/src/main/java/com/example/simonbeaulieu/moondj/CentralActivity.java
@@ -0,0 +1,13 @@
+package com.example.simonbeaulieu.moondj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CentralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_central);
+    }
+}
