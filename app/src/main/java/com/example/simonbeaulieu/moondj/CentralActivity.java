e3ce6f86658c6f841bbf253f50500d1dde2bd7c5
macvegas <macvegas7@gmail.com>
2017-08-02 16:04:22 +0200

    safe commit

--- app/src/main/java/com/example/simonbeaulieu/moondj/CentralActivity.java
+++ app/src/main/java/com/example/simonbeaulieu/moondj/CentralActivity.java
@@ -3,11 +3,12 @@ package com.example.simonbeaulieu.moondj;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CentralActivity extends AppCompatActivity {
+public class CentralActivity extends HeritageActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_central);
+        System.out.println(songArrayList);
     }
 }
---
+++ app/src/main/java/com/example/simonbeaulieu/moondj/Song.java
@@ -0,0 +1,52 @@
+package com.example.simonbeaulieu.moondj;
+
+import java.io.Serializable;
+
+/**
+ * Created by simon.beaulieu on 28/07/2017.
+ */
+public class Song implements Serializable {
+
+    private String path;
+    private String title;
+    private String artist;
+    private String notation;
+//constructeur avec notation
+    public Song(String path, String title, String artist,String notation) {
+        this.path = path;
+        this.title = title;
+        this.artist = artist;
+        this.notation=notation;
+    }
+//constructeur sans notation
+    public Song(String path, String title, String artist) {
+        this.path = path;
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist ;
+    }
+
+    public void setNotation(String notation) {
+        this.notation = notation;
+    }
+
+    public String getNotation() {
+        return notation;
+    }
+}
+
