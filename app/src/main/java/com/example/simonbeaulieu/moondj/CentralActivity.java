755b719d6309baadb6520491e7ea2d754c069b6b
macvegas <macvegas7@gmail.com>
2017-08-03 14:32:34 +0200

    switch database vers l'heritageActivity

--- app/src/main/java/com/example/simonbeaulieu/moondj/CentralActivity.java
+++ app/src/main/java/com/example/simonbeaulieu/moondj/CentralActivity.java
@@ -14,39 +14,8 @@ public class CentralActivity extends HeritageActivity {
         setContentView(R.layout.activity_central);
 
 
-        DBHandler dbHandler= new DBHandler(this);
-        setDBup(dbHandler);
-        dbHandler.close();
-    }
-
-    public void setDBup(DBHandler dbHandler){
-
-        //checking if the table of musics already exists or not
-        SQLiteDatabase db= dbHandler.getWritableDatabase();
-        Cursor c = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
-        boolean isCreated = false;
-        if (c.moveToFirst()) {
-            while ( !c.isAfterLast() ) {
-                if(c.getString(0).equals("musiques")){
-                    Log.d("CentralActivity","table déja créée");
-                    isCreated=true;
-                }
-                c.moveToNext();
-            }
-        }
-        c.close();
-        if(!isCreated){
-            dbHandler.onCreate(dbHandler.getWritableDatabase());
-            Log.d("CentralActivity","recréation de la table");
-        }
-        dbHandler.addSong(new Song("/path/interressant","Amazing","INNA","0.0"));
-        dbHandler.addSong(new Song("/path/coucou","Babar","polo","0.0"));
-//        dbHandler.deleteSong("Amazing");
-        int count= dbHandler.getSongCount();
-        echo("count = "+count);
-        Song song =dbHandler.getSong("Amazing");
-        echo("id is: "+song.getId());
-//        dbHandler.dropTableSong();
 
     }
+
+
 }
--- app/src/main/java/com/example/simonbeaulieu/moondj/HeritageActivity.java
+++ app/src/main/java/com/example/simonbeaulieu/moondj/HeritageActivity.java
@@ -1,7 +1,10 @@
 package com.example.simonbeaulieu.moondj;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -16,7 +19,42 @@ public class HeritageActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DBHandler dbHandler= new DBHandler(this);
+        setDBup(dbHandler);
+        dbHandler.close();
     }
+
+    public void setDBup(DBHandler dbHandler){
+
+        //checking if the table of musics already exists or not
+        SQLiteDatabase db= dbHandler.getWritableDatabase();
+        Cursor c = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+        boolean isCreated = false;
+        if (c.moveToFirst()) {
+            while ( !c.isAfterLast() ) {
+                if(c.getString(0).equals("musiques")){
+                    Log.d("CentralActivity","table déja créée");
+                    isCreated=true;
+                }
+                c.moveToNext();
+            }
+        }
+        c.close();
+        if(!isCreated){
+            dbHandler.onCreate(dbHandler.getWritableDatabase());
+            Log.d("CentralActivity","recréation de la table");
+        }
+        dbHandler.addSong(new Song("/path/interressant","Amazing","INNA","0.0"));
+        dbHandler.addSong(new Song("/path/coucou","Babar","polo","0.0"));
+//        dbHandler.deleteSong("Amazing");
+        int count= dbHandler.getSongCount();
+        echo("count = "+count);
+        Song song =dbHandler.getSong("Amazing");
+        echo("id is: "+song.getId());
+//        dbHandler.dropTableSong();
+
+    }
+
 
     public static void echo(Object o){
         System.out.println(o);
