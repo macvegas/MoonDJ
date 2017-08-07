66219df1dfce203aed1e840ce724f4c305c9300f
macvegas <macvegas7@gmail.com>
2017-08-07 15:12:21 +0200

    transformation du mainmenu en fragment dynamique

--- app/src/main/java/com/example/simonbeaulieu/moondj/CentralActivity.java
+++ app/src/main/java/com/example/simonbeaulieu/moondj/CentralActivity.java
@@ -16,7 +16,11 @@ public class CentralActivity extends HeritageActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_central);
-
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        android.app.FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        Frag_menuPrincipal fragMenuPrincipal = new Frag_menuPrincipal();
+        fragmentTransaction.replace(R.id.fragmentLayout,fragMenuPrincipal);
+        fragmentTransaction.commit();
 
 //        for(Song i: songArrayList){
 //            echo(i.getTitle() + " || "+i.getArtist());
---
+++ app/src/main/java/com/example/simonbeaulieu/moondj/Frag_menuPrincipal.java
@@ -0,0 +1,22 @@
+package com.example.simonbeaulieu.moondj;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by simon.beaulieu on 07/08/2017.
+ */
+
+public class Frag_menuPrincipal extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mainmenu,
+                container, false);
+        return view;
+    }
+
+}
