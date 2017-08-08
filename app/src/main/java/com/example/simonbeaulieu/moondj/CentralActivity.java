6d5ba51abe0f1db8f1b907a75db61e86e585758f
macvegas <macvegas7@gmail.com>
2017-08-08 12:04:54 +0200

    comments and such

--- app/src/main/java/com/example/simonbeaulieu/moondj/CentralActivity.java
+++ app/src/main/java/com/example/simonbeaulieu/moondj/CentralActivity.java
@@ -13,14 +13,25 @@ public class CentralActivity extends HeritageActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_central);
+        //mise en place du fragment principal
         FragmentTransaction ft=getFT(this);
         Frag_menuPrincipal fragMenuPrincipal = new Frag_menuPrincipal();
         MainFragment.instanciate(fragMenuPrincipal,ft,true,false,R.id.fragmentLayout);
+        setViews();
 
-        imageView=(ImageView)findViewById(R.id.gif) ;
-        Glide.with(this).load(R.drawable.loading).into(imageView);
+
+
+
 //        for(Song i: songArrayList){
 //            echo(i.getTitle() + " || "+i.getArtist());
 //        }
+    }
+
+    public void setViews(){
+        //display du gif bloby
+        imageView=(ImageView)findViewById(R.id.gif) ;
+        Glide.with(this).load(R.drawable.loading).into(imageView);
+
+
     }
 }
