f96516ad7ad21a232352536353ed27a8f1e4c650
macvegas <macvegas7@gmail.com>
2017-08-18 19:21:31 +0200

    tonextsong implémenté

--- app/src/main/java/com/example/simonbeaulieu/moondj/MusicService.java
+++ app/src/main/java/com/example/simonbeaulieu/moondj/MusicService.java
@@ -7,6 +7,8 @@ import android.os.IBinder;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.Random;
+
 import android.content.ContentUris;
 import android.media.AudioManager;
 import android.net.Uri;
@@ -23,22 +25,28 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     private MediaPlayer player;
     //song list
     ArrayList<Song> songs;
+    //list already played musics
+    ArrayList<Song> songsAlreadyPlayed=new ArrayList<>();
     private int songPosn;
     //binder qui arrive sur le service et provenant de l'activité.
     private final IBinder musicBind = new MusicBinder();
     //position de lecture
     private int musicTimePosition;
-
+    //activité
+    CentralActivity centralActivity;
 
     public void onCreate(){
         //creates the service
         super.onCreate();
+        //gets activity
+        centralActivity=(CentralActivity)HeritageActivity.getCurrentActivityInstance();
         //initialize position
         songPosn=0;
         //creates a player
         player=new MediaPlayer();
         //initialize the mediaplayer the way we want it to be
         initMusicPlayer();
+
     }
 
     public void initMusicPlayer(){
@@ -64,6 +72,7 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     public boolean onUnbind(Intent intent) {// TODO: 16/08/17 This will execute when the user exits the app, at which point we will stop the service. 
         player.stop();
         player.release();
+        songsAlreadyPlayed=null;
         return false;
     }
 
@@ -106,14 +115,14 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
         player.reset();
         //get the song
         Song playSong = songs.get(songPosn);
-//        long currSong =playSong.getId();
-//        Uri trackUri=ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,currSong);
         try{
             player.setDataSource(playSong.getPath());
         }catch (Exception e){
             Log.e("MUSIC SERVICE","Error setting data source",e);
         }
         player.prepareAsync();
+        //ajoute la musique a la liste des musiques jouées
+        songsAlreadyPlayed.add(playSong);
     }
 
     public void pauseSong(){
@@ -126,11 +135,43 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
 
     public void toNextSong(){
         // TODO: 18/08/17 faire le cas random, auquel on joint le cas "isplaying"
+        boolean wasplaying = getIsPlaying();
+
+        // si toutes les musiques ont été jouées, liste remise a zero
+        if(songsAlreadyPlayed.size()==songs.size()){
+            songsAlreadyPlayed=new ArrayList<>();
+        }
+        //cas ou la musique était en cours de lecture
+        player.stop();
+        //lecture random activée
+        if(centralActivity.randomIsActivated){
+            //donne un random qui n'a pas déja été joué
+            Random r=new Random();
+            while (songsAlreadyPlayed.contains(songs.get(songPosn))){
+                songPosn= r.nextInt(songs.size());
+            }
+        }
+        // cas ou on est dans une lecture NON random
+        else {
+            songPosn += 1;
+            //verification qu'on n'est pas après la dernière musique. (au dela de la liste)
+            if (songPosn >= songs.size()) {
+                //reprise a 0
+                songPosn = 0;
+            }
+        }
+        //selectionne et lance la musique suivante
+        setSong(songPosn);
+        //si la lecture était activée
+        if (wasplaying){playSong();}
     }
 
     public void toPreviousSong(){
-
         // TODO: 18/08/17 faire le previous en réfléchissant a comment jouer la musique précédente en random (faire une liste des musiques a mesure qu'on les joues?)
+    }
+
+    public boolean getIsPlaying(){
+        return player.isPlaying();
     }
 
 
